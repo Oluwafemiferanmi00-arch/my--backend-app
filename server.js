aee36bb2e148ccadc263e3bdd57c93cbe78b85f2
Oluwafemiferanmi00-arch <Oluwafemiferanmi00@gmail.com>
2026-04-02 12:35:52 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const express = require("express");
 const app = express();
 
 app.use(express.json());
